f94cbbe06a6946ce14ac9a11da072ce75b17c338
mor <odkarit22@gmail.com>
2019-07-17 12:14:39 +0300

    trying to work with a main function

---
+++ 23who wants to be a millionaire/testing.js
@@ -0,0 +1,144 @@
+"use strict";
+var questionCounter = 0;
+var questions;
+var pickedAnswers = [];
+var remainingTime;
+var timer;
+
+const promise = fetch('main.json').then(res => res.json()).then((data) => {
+   questions = main([...data]);
+    /* 
+    // this line sends the array (to the shuffleQuestionsAnswers function) by refference and not by value. // not good!
+    shuffleQuestionsAnswers(data);
+ 
+    // this line sends the array (to the shuffleQuestionsAnswers function) by value and not by refference. // good!
+    shuffleQuestionsAnswers([...data]);
+    
+    */
+}).catch(err => { throw err });
+
+function main(questions) {
+    console.log(questions);
+    let newQuestions = shuffle(questions);
+    for (let i = 0; i < newQuestions.length; i++) {
+        const question = newQuestions[i];
+        question.a = shuffle(question.a);
+    }
+
+    displayCurrentQuestion();
+    startCountDown();
+
+    remainingTime = 60;
+    updateTimerView();
+    clearInterval(timer);
+    timer = setInterval(function () {
+        remainingTime--;
+        updateTimerView();
+        if (remainingTime === 0) {
+            pickedAnswers.push(-1);
+            questionCounter++;
+            if (questionCounter < questions.length) {
+                displayCurrentQuestion();
+                moveToNextQuestion();
+            }
+            else {
+                finishGame();
+            }
+        }
+    }, 1000)
+
+
+}
+
+/* function shuffleQuestionsAnswers(questions) {
+    let newQustions = shuffle(questions);
+    for (let i = 0; i < newQuestions.length; i++) {
+        const question = newQuestions[i];
+        question.a = shuffle(question.a);
+    }
+
+    return newQuestions;
+} */
+
+function shuffle(arr) {
+    let newArr = [];
+    while (arr.length > 0) {
+        let index = Number(Math.floor(Math.random() * arr.length));
+        let item = arr.splice(index, 1)[0];
+        newArr.push(item);
+    }
+    return newArr;
+}
+
+function displayCurrentQuestion() {
+    let questionObj = questions[questionCounter];
+    let questionElement = document.getElementById('question');
+    questionElement.innerHTML = questionObj.q;
+    questionObj.a.forEach(function (answerObj, i) {
+        var answerElement = document.getElementById('answer' + i);
+        answerElement.innerHTML = answerObj.aText;
+        answerElement.addEventListener("click", onAnswerClick);
+    });
+}
+
+function onAnswerClick(element) {
+    let pickedAnswerIndex = Number(element.target.id.substring("answer".length));
+    pickedAnswers.push(pickedAnswerIndex);
+}
+
+/* function moveToNextQuestion() {
+    questionCounter++;
+    if (questionCounter < questions.length) {
+        displayCurrentQuestion();
+    }
+    else {
+        finishGame();
+    }
+} */
+
+/* function startCountDown() {
+    remainingTime = 60;
+    updateTimerView();
+    clearInterval(timer);
+    timer = setInterval(function () {
+        remainingTime--;
+        updateTimerView();
+        if (remainingTime === 0) {
+            pickedAnswers.push(-1);
+            moveToNextQuestion();
+        }
+    }, 1000)
+} */
+
+function updateTimerView() {
+    let remainingTimeElement = document.getElementById('remainingTime');
+    remainingTimeElement.innerHTML = 'time remaining: ' + remainingTime;
+}
+
+function finishGame() {
+    clearInterval(timer);
+    let correctAnswers = 0;
+    questions.forEach(function (question, i) {
+        let pickedAnswerIndex = pickedAnswers[i];
+        if (pickedAnswerIndex !== -1 && question.a[pickedAnswerIndex].isTrue) {
+            correctAnswers++;
+        }
+    });
+    // removeGameBox(correctAnswers);
+    alert("You were right in " + correctAnswers + "\nand wrong in " + (questions.length - correctAnswers) + "\nout of " + questions.length + " questions.");
+}
+
+/*  function removeGameBox(correctAnswers) {
+    var gameBox = document.getElementById('gameBox');
+    document.body.removeChild(gameBox);
+
+    var result = document.createElement('div');
+    result.id = 'resultBox';
+    document.body.appendChild(result);
+    result.innerHTML = "You were right in " + correctAnswers + " and wrong in " + (questions.length - correctAnswers) + " out of " + questions.length + " questions.";
+
+    var startNewButton = document.createElement('button');
+    startNewButton.id = 'startNewButton';
+    document.body.appendChild(startNewButton);
+    startNewButton.innerHTML = 'Start A New Game';
+}  */
