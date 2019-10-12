b5bb950bd482152a237fed9be2867dbb1f30593d
Dor Elmaliach <dor.elmal11@gmail.com>
2019-10-12 14:54:52 +0300

    order tickets - date filter

--- order-tickets/src/components/FilterSection.js
+++ order-tickets/src/components/FilterSection.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+import React, {useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
 import FilterDropdown from "./FilterDropdown";
+import actionTypes from "../actionTypes";
+import {getDayString} from "../dateUtils";
 
 function FilterSection(props) {
   let cities = useSelector(state => state && state.cities);
+  let [departureDate, setDepartureDate] = useState(null);
+  let dispatch = useDispatch();
+
+  function onDepartureDateChange(event) {
+    let value = event.target.value;
+    let parsedDate = getDayString(new Date(value));
+    setDepartureDate(value);
+    dispatch({
+      type: actionTypes.FILTER_CHANGE,
+      filteredField: "departureDate",
+      value: parsedDate
+    });
+  }
 
   return <div className={"filter-section"}>
     <h1>Bus Tickets</h1>
-    <FilterDropdown filteredField={"fromCity"} options={cities} placeholder={"From"} />
-    <FilterDropdown filteredField={"toCity"} options={cities} placeholder={"To"} />
+    <FilterDropdown filteredField={"fromCity"} options={cities} placeholder={"From"}/>
+    <FilterDropdown filteredField={"toCity"} options={cities} placeholder={"To"}/>
+
+    <input type="date"
+           name={"departurePicker"}
+           id={"departurePicker"}
+           onChange={e => onDepartureDateChange(e)}
+           value={departureDate}/>
   </div>;
 }
 
